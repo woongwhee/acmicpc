9c6f7a2184f4b4eb1a47a2f936cf2759429fb772
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-09-27 19:00:07 +0900

    Update and rename problem10891clear.java to problem10819clear.java

--- exhaustivesearch/problem10819clear.java
+++ exhaustivesearch/problem10819clear.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class problem10891clear {
+public class problem10819clear {
         public static void main(String[] args) throws IOException {
             BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
             int length=Integer.parseInt(br.readLine());

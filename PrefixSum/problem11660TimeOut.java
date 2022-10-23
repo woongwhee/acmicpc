17b8c00a94e4333a8ba3742b2d602765d6f65212
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-10-23 21:38:20 +0900

    Week4 prefixsum

--- PrefixSum/problem11660TimeOut.java
+++ PrefixSum/problem11660TimeOut.java
@@ -5,44 +5,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class problem11660 {
-
+public class problem11660TimeOut {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(st.nextToken())+1;
+        int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
         int[][] arr=new int[N][N];
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             st=new StringTokenizer(br.readLine());
-            for (int j = 1; j < N; j++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 2; i < N; i++) {
-            for (int j = 1; j < N; j++) {
-                arr[i][j]=arr[i-1][j]+arr[i][j];
-            }
-        }
-        for (int i = 1; i < N; i++) {
-            for (int j = 2; j < N; j++) {
-                arr[i][j]=arr[i][j-1]+arr[i][j];
-            }
-        }
+        int startX[]=new int[M];
+        int startY[]=new int[M];
+        int endX[]=new int[M];
+        int endY[]=new int[M];
         StringBuffer sb=new StringBuffer();
         for (int i = 0; i < M; i++) {
-
             st=new StringTokenizer(br.readLine());
-            int startX=Integer.parseInt(st.nextToken());
-            int startY=Integer.parseInt(st.nextToken());
-            int endX=Integer.parseInt(st.nextToken());
-            int endY=Integer.parseInt(st.nextToken());
-            int result=arr[startX-1][startY-1]+arr[endX][endY]-arr[endX][startY-1]-arr[startX-1][endY];
+            startX[i]=Integer.parseInt(st.nextToken())-1;
+            startY[i]=Integer.parseInt(st.nextToken())-1;
+            endX[i]=Integer.parseInt(st.nextToken())-1;
+            endY[i]=Integer.parseInt(st.nextToken())-1;
+
+        }
+        int result=0;
+        for (int i = 0; i < M; i++) {
+            for (int j = startX[i]; j <= endX[i]; j++) {
+                for (int k=startY[i]; k <= endY[i]; k++) {
+                    result+=arr[j][k];
+                }
+            }
             sb.append(result);
             sb.append("\n");
         }
         System.out.println(sb.toString());
-
-
     }
 }

3826b9b331445d5971a78c4bd08dba4ec17c0fbd
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-12-06 00:41:52 +0900

    1로만들기

---
+++ bfs/problem24444.java
@@ -0,0 +1,57 @@
+package net.acmicpc.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class problem24444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        int Start=Integer.parseInt(st.nextToken());
+        LinkedList<Integer>[] vertexList=new LinkedList[N+1];
+        for (int i = 1; i <= N; i++) {
+            vertexList[i]=new LinkedList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st=new StringTokenizer(br.readLine());
+            int to=Integer.parseInt(st.nextToken());
+            int from=Integer.parseInt(st.nextToken());
+            vertexList[to].add(from);
+            vertexList[from].add(to);
+        }
+        System.out.println(bfs(Start,vertexList));
+
+
+
+    }
+    static String bfs(int start,LinkedList<Integer>[] vertexList){
+        int[] orderArr=new int[vertexList.length];
+        int order=1;
+        boolean[] visited=new boolean[vertexList.length];
+        Queue<Integer> queue=new LinkedList<>();
+        visited[start]=true;
+        queue.add(start);
+        while (!queue.isEmpty()){
+            int cur=queue.poll();
+            LinkedList<Integer> vertex=vertexList[cur];
+            orderArr[cur]=order++;
+            Collections.sort(vertex);
+            for (Integer i:vertex) {
+                if(!visited[i]){
+                    queue.add(i);
+                    visited[i]=true;
+                }
+            }
+        }
+        StringBuffer sb=new StringBuffer();
+        for (int i = 1; i < orderArr.length; i++) {
+            sb.append(orderArr[i]);
+            if(i!=orderArr.length-1)sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ dynamic/problem1463.java
@@ -0,0 +1,26 @@
+package net.acmicpc.dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class problem1463 {
+    static int min;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        int[] dp=new int[N+1];
+        for (int i = 2; i <=N ; i++) {
+            dp[i] = dp[i-1]+1;
+            if(i%3==0){
+                dp[i]=Math.min(dp[i],dp[i/3]+1);
+
+            }
+            if(i%2==0){
+                dp[i]=(Math.min(dp[i],dp[i/2]+1));
+            }
+        }
+        System.out.println(dp[N]);
+    }
+
+}

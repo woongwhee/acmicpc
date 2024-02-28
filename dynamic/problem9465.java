8bc2f88020f4a56827402d8e335c2815c8aa0b83
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2024-02-29 06:59:53 +0900

    스티커 스티커

---
+++ dynamic/problem9465.java
@@ -0,0 +1,10 @@
+package net.acmicpc.dynamic;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class problem9465 {
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    }
+}
